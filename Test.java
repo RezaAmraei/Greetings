bb1799e61e624498d31dd3b7f3813b1db41046a3
Reza Amraei <amraeireza@gmail.com>
2022-04-03 23:23:18 -0700

    first commit

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("My name is Reza Amraei");
+        System.out.println("I am 22 years olf");
+        System.out.println("My hometown is Torrance, Ca");
+    }
+}
